5b33b81d9f8599c61aa6e3e938b23523ccc2464a
amordoch <whiskeyadam@yahoo.com>
2014-03-13 21:16:59 -0400

    Update LookupTable
    Added two new Maps to LookupTable containing atomic number and element
    names, edited getElementInfo() to correspond to changes.

--- src/com/mordoch/chemtools/LookupTable.java
+++ src/com/mordoch/chemtools/LookupTable.java
@@ -25,7 +25,7 @@ import java.util.HashMap;
 /**
  * This class is used to store information about elements.
  * @author Ariel Mordoch
- * @version 1.0
+ * @version 1.1
  * @since 1.0.3
  */
 
@@ -35,7 +35,7 @@ public class LookupTable {
 	 * Prints a list of all the tables contained in the class.
 	 * @since 1.0.3
 	 */
-	
+
 	public void list() {
 		System.out.println("Lists in lookup table:");
 		System.out.println( molarMass().toString() );
@@ -43,7 +43,7 @@ public class LookupTable {
 
 	/**
 	 * Finds information about an element and then returns a list containing that information.
-	 * <p>Format: <code>atomic mass, molar mass, atomic number</code> </p> 
+	 * <p> Format: name, atomic number, atomic mass, molar mass </p> 
 	 * @param element a string containing the symbol for an element.
 	 * @return A List<<x>String> containing information about an element. See format above.
 	 * @since 1.0.3
@@ -53,11 +53,20 @@ public class LookupTable {
 		// Create a new ArrayList that stores element info.
 		List<String> elementInfo = new ArrayList<String>();
 		// Print element info to console
+		System.out.println("Name: " + name().get(element) );
+		System.out.println("Atomic number: " + atomicNumber().get(element) );
 		System.out.println("Atomic mass: " + molarMass().get(element) + " amu" );
 		System.out.println("Molar mass: " + molarMass().get(element) + " g/mol" );
-		// Store element info
+		/* Store element info */
+		// Name
+		elementInfo.add( name().get(element) );
+		// Atomic number
+		elementInfo.add( Integer.toString( atomicNumber().get(element), 10 ) );
+		// Atomic mass
 		elementInfo.add( Double.toString( molarMass().get(element) ) );
+		// Molar mass
 		elementInfo.add( Double.toString( molarMass().get(element) ) );
+		// Return the list
 		return elementInfo;
 	}
 
@@ -193,5 +202,263 @@ public class LookupTable {
 		return molarMass;
 	}
 
+	/**
+	 * Defines and returns a Map containing elements and their corresponding atomic numbers.
+	 * @return A Map containing elements and their atomic numbers.
+	 * @since 1.0.3
+	 */
 
+	public final Map<String, Integer> atomicNumber() {
+		Map<String, Integer> atomicNumber = new HashMap<String, Integer>();
+		atomicNumber.put("H",  1);
+		atomicNumber.put("He", 2);
+		atomicNumber.put("Li", 3);
+		atomicNumber.put("Be", 4);
+		atomicNumber.put("B",  5);
+		atomicNumber.put("C",  6);
+		atomicNumber.put("N",  7);
+		atomicNumber.put("O",  8);
+		atomicNumber.put("F",  9);
+		atomicNumber.put("Ne", 10);
+		atomicNumber.put("Na", 11);
+		atomicNumber.put("Mg", 12);
+		atomicNumber.put("Al", 13);
+		atomicNumber.put("Si", 14);
+		atomicNumber.put("P",  15); 
+		atomicNumber.put("S",  16);
+		atomicNumber.put("Cl", 17);
+		atomicNumber.put("Ar", 18);
+		atomicNumber.put("K",  19);
+		atomicNumber.put("Ca", 20);
+		atomicNumber.put("Sc", 21);
+		atomicNumber.put("Ti", 22);
+		atomicNumber.put("V",  23);
+		atomicNumber.put("Cr", 24);
+		atomicNumber.put("Mn", 25);
+		atomicNumber.put("Fe", 26);
+		atomicNumber.put("Co", 27);
+		atomicNumber.put("Ni", 28);
+		atomicNumber.put("Cu", 29);
+		atomicNumber.put("Zn", 30);
+		atomicNumber.put("Ga", 31);
+		atomicNumber.put("Ge", 32);
+		atomicNumber.put("As", 33);
+		atomicNumber.put("Se", 34);
+		atomicNumber.put("Br", 35);
+		atomicNumber.put("Kr", 36);
+		atomicNumber.put("Rb", 37);
+		atomicNumber.put("Sr", 38);
+		atomicNumber.put("Y",  39);
+		atomicNumber.put("Zr", 40);
+		atomicNumber.put("Nb", 41);
+		atomicNumber.put("Mo", 42);
+		atomicNumber.put("Tc", 43);
+		atomicNumber.put("Ru", 44);
+		atomicNumber.put("Rh", 45);
+		atomicNumber.put("Pd", 46);
+		atomicNumber.put("Ag", 47);
+		atomicNumber.put("Cd", 48);
+		atomicNumber.put("In", 49);
+		atomicNumber.put("Sn", 50);
+		atomicNumber.put("Sb", 51);
+		atomicNumber.put("Te", 52);
+		atomicNumber.put("I",  53);
+		atomicNumber.put("Xe", 54);
+		atomicNumber.put("Cs", 55);
+		atomicNumber.put("Ba", 56);
+		atomicNumber.put("Hf", 72);
+		atomicNumber.put("Ta", 73);
+		atomicNumber.put("W",  74);
+		atomicNumber.put("Re", 75);
+		atomicNumber.put("Os", 76);
+		atomicNumber.put("Ir", 77);
+		atomicNumber.put("Pt", 78);
+		// I LOVE GOOOOOOOOOOOOLD
+		atomicNumber.put("Au", 79);
+		atomicNumber.put("Hg", 80);
+		atomicNumber.put("Tl", 81);
+		atomicNumber.put("Pb", 82);
+		atomicNumber.put("Bi", 83);
+		atomicNumber.put("Po", 84);
+		atomicNumber.put("At", 85);
+		atomicNumber.put("Rn", 86);
+		atomicNumber.put("Fr", 87);
+		atomicNumber.put("Ra", 88);
+		atomicNumber.put("Rf", 104);
+		atomicNumber.put("Db", 105);
+		atomicNumber.put("Sg", 106);
+		atomicNumber.put("Bh", 107);
+		atomicNumber.put("Hs", 108);
+		atomicNumber.put("Mt", 109);
+		atomicNumber.put("Ds", 110);
+		atomicNumber.put("Rg", 111);
+		atomicNumber.put("Cn", 112);
+		atomicNumber.put("Uut", 113);
+		atomicNumber.put("Fl", 114);
+		atomicNumber.put("Uup", 115);
+		atomicNumber.put("Lv", 116);
+		atomicNumber.put("Uus", 117);
+		atomicNumber.put("Uuo", 118);
+		atomicNumber.put("La", 57);
+		atomicNumber.put("Ce", 58);
+		atomicNumber.put("Pr", 59);
+		atomicNumber.put("Nd", 60);
+		atomicNumber.put("Pm", 61);
+		atomicNumber.put("Sm", 62);
+		atomicNumber.put("Eu", 63);
+		atomicNumber.put("Gd", 64);
+		atomicNumber.put("Tb", 65);
+		atomicNumber.put("Dy", 66);
+		atomicNumber.put("Ho", 67);
+		atomicNumber.put("Er", 68);
+		atomicNumber.put("Tm", 69);
+		atomicNumber.put("Yb", 70);
+		atomicNumber.put("Lu", 71);
+		atomicNumber.put("Ac", 89);
+		atomicNumber.put("Th", 90);
+		atomicNumber.put("Pa", 91);
+		atomicNumber.put("U",  92);
+		atomicNumber.put("Np", 93);
+		atomicNumber.put("Pu", 94);
+		atomicNumber.put("Am", 95);
+		atomicNumber.put("Cm", 96);
+		atomicNumber.put("Bk", 97);
+		atomicNumber.put("Cf", 98);
+		atomicNumber.put("Es", 99);
+		atomicNumber.put("Fm", 100);
+		atomicNumber.put("Md", 101);
+		atomicNumber.put("No", 102);
+		atomicNumber.put("Lr", 103);
+		return atomicNumber;
+	}
+
+	/**
+	 * Defines and returns a Map containing elements and their corresponding names.
+	 * @return A Map containing elements and their names.
+	 * @since 1.0.3
+	 */
+
+	public final Map<String, String> name() {
+		Map<String, String> name = new HashMap<String, String>();
+		name.put("H",  "Hydrogen");
+		name.put("He", "Helium");
+		name.put("Li", "Lithuim");
+		name.put("Be", "Beryllium");
+		name.put("B",  "Boron");
+		name.put("C",  "Carbon");
+		name.put("N",  "Nitrogren");
+		name.put("O",  "Oxygen");
+		name.put("F",  "Flourine");
+		name.put("Ne", "Neon");
+		name.put("Na", "Sodium");
+		name.put("Mg", "Magnesium");
+		name.put("Al", "Aluminium");
+		name.put("Si", "Silicon");
+		name.put("P",  "Phosphorus"); 
+		name.put("S",  "Sulfur");
+		name.put("Cl", "Chlorine");
+		name.put("Ar", "Argon");
+		name.put("K",  "Potassium");
+		name.put("Ca", "Calcium");
+		name.put("Sc", "Scandium");
+		name.put("Ti", "Titanium");
+		name.put("V",  "Vanadium");
+		name.put("Cr", "Chromium");
+		name.put("Mn", "Manganese");
+		name.put("Fe", "Iron");
+		name.put("Co", "Cobalt");
+		name.put("Ni", "Nickel");
+		name.put("Cu", "Copper");
+		name.put("Zn", "Zinc");
+		name.put("Ga", "Gallium");
+		name.put("Ge", "Germanium");
+		name.put("As", "Arsenic");
+		name.put("Se", "Selinium");
+		name.put("Br", "Bromine");
+		name.put("Kr", "Krypton");
+		name.put("Rb", "Rubidium");
+		name.put("Sr", "Strontium");
+		name.put("Y",  "Yttrium");
+		name.put("Zr", "Zirconium");
+		name.put("Nb", "Niobium");
+		name.put("Mo", "Molybdenum");
+		name.put("Tc", "Technetium");
+		name.put("Ru", "Ruthenium");
+		name.put("Rh", "Rhodium");
+		name.put("Pd", "Palladium");
+		name.put("Ag", "Silver");
+		name.put("Cd", "Cadmium");
+		name.put("In", "Indium");
+		name.put("Sn", "Tin");
+		name.put("Sb", "Antimony");
+		name.put("Te", "Tellurium");
+		name.put("I",  "Iodine");
+		name.put("Xe", "Xenon");
+		name.put("Cs", "Ceaseium");
+		name.put("Ba", "Barium");
+		name.put("Hf", "Hafnium");
+		name.put("Ta", "Tantalum");
+		name.put("W",  "Tungsten");
+		name.put("Re", "Rhenium");
+		name.put("Os", "Osmium");
+		name.put("Ir", "Iridium");
+		name.put("Pt", "Platinum");
+		// I LOVE GOOOOOOOOOOOOLD
+		name.put("Au", "Gold");
+		name.put("Hg", "Mercury");
+		name.put("Tl", "Thallium");
+		name.put("Pb", "Lead");
+		name.put("Bi", "Bismuth");
+		name.put("Po", "Polonium");
+		name.put("At", "Astaline");
+		name.put("Rn", "Radon");
+		name.put("Fr", "Francium");
+		name.put("Ra", "Radium");
+		name.put("Rf", "Rutherfordium");
+		name.put("Db", "Dubnium");
+		name.put("Sg", "Seaborgium");
+		name.put("Bh", "Bohrium");
+		name.put("Hs", "Hassium");
+		name.put("Mt", "Meitnerium");
+		name.put("Ds", "Darmstadtium");
+		name.put("Rg", "Roentgenium");
+		name.put("Cn", "Copernicium");
+		name.put("Uut", "Unutrium");
+		name.put("Fl", "Flevroium");
+		name.put("Uup", "Unupentium");
+		name.put("Lv", "Livermorium");
+		name.put("Uus", "Unuseptium");
+		name.put("Uuo", "Ununoctium");
+		name.put("La", "Lanthanum");
+		name.put("Ce", "Cerium");
+		name.put("Pr", "Praseodymium");
+		name.put("Nd", "Neodymium");
+		name.put("Pm", "Promethium");
+		name.put("Sm", "Samarium");
+		name.put("Eu", "Europium");
+		name.put("Gd", "Gadolinium");
+		name.put("Tb", "Terbium");
+		name.put("Dy", "Dysprosium");
+		name.put("Ho", "Holmium");
+		name.put("Er", "Erbium");
+		name.put("Tm", "Thulium");
+		name.put("Yb", "Yttrbium");
+		name.put("Lu", "Lutetium");
+		name.put("Ac", "Actinium");
+		name.put("Th", "Thorium");
+		name.put("Pa", "Protactinium");
+		name.put("U",  "Uranium");
+		name.put("Np", "Neptunium");
+		name.put("Pu", "Plutonium");
+		name.put("Am", "Americium");
+		name.put("Cm", "Curium");
+		name.put("Bk", "Berkelium");
+		name.put("Cf", "Califonium");
+		name.put("Es", "Einsteinium");
+		name.put("Fm", "Fermium");
+		name.put("Md", "Mendelvium");
+		name.put("No", "Nobelium");
+		name.put("Lr", "Lawrenium");
+		return name;
+	}
 }
